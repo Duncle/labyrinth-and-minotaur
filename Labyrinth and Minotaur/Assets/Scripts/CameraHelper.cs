64f37c562ec522f197c3a67f10a79dc87ace572f
Светослав <Svift431@gmail.com>
2022-11-04 15:41:56 +0300

    Some Changes

--- Labyrinth and Minotaur/Assets/Scripts/CameraHelper.cs
+++ Labyrinth and Minotaur/Assets/Scripts/CameraHelper.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CameraHelper : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    public Animator playerAnimator;
     public float rotSpeed = 1.5f;
     private float _rotY;
     private float _rotX;
+    private float _rotYTemp;
     private Vector3 _offset;
     void Start()
     {
@@ -18,22 +20,32 @@ public class CameraHelper : MonoBehaviour
     }
     void Update()
     {
-        _rotX += Input.GetAxis("Mouse X") * rotSpeed * 3;
-        _rotY += Input.GetAxis("Mouse Y") * -2;
+        if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("BasicMotions@Jump01 [RM]"))
+        {
+            _rotX += Input.GetAxis("Mouse X") * rotSpeed * 3;
+            _rotY += Input.GetAxis("Mouse Y") * -2;
 
-        if (_rotY >= 26)
-        {
-            _rotY = 26;
+            if (_rotY >= 26)
+            {
+                _rotY = 26;
+            }
+            if (_rotY <= -33)
+            {
+                _rotY = -33;
+            }
+            if (_rotY <= 26 && _rotY >= -33)
+            {
+                Debug.Log("asd");
+                Quaternion rotation = Quaternion.Euler(_rotY, _rotX, 0);
+                transform.rotation = (rotation);
+            }
+            _rotYTemp = _rotY;
         }
-        if (_rotY <= -33)
+        else
         {
-            _rotY = -33;
-        }
-        if (_rotY <= 26 && _rotY >= -33)
-        {
-            Debug.Log("asd");
-            Quaternion rotation = Quaternion.Euler(_rotY, _rotX, 0);
+            Quaternion rotation = Quaternion.Euler(_rotYTemp, _rotX, 0);
             transform.rotation = (rotation);
         }
+       
     }
 }
