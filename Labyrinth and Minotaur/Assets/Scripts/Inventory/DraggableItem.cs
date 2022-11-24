3785757c5c226b4386318f8407357a5e1f01ea34
Duncle <solekawp1488@gmail.com>
2022-11-25 01:47:20 +0300

    Set Grid Group and Improve d'n'd

--- Labyrinth and Minotaur/Assets/Scripts/Inventory/DraggableItem.cs
+++ Labyrinth and Minotaur/Assets/Scripts/Inventory/DraggableItem.cs
@@ -5,18 +5,25 @@ using UnityEngine.EventSystems;
 
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("BeginDrag");
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("Dragging");
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("End drag");
-    }
+    Transform parentAfterDrag;
+        
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            Debug.Log("BeginDrag");
+            parentAfterDrag = transform.parent;
+            transform.SetParent(transform.root);
+            transform.SetAsLastSibling();
+        }
+    
+        public void OnDrag(PointerEventData eventData)
+        {
+            Debug.Log("Dragging");
+            transform.position = Input.mousePosition;
+        }
+    
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            Debug.Log("End drag");
+            transform.SetParent(parentAfterDrag);
+        }
 }
