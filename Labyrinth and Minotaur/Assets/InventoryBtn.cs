5d5413b5177b6b57119efc67872b256732c99002
Duncle <solekawp1488@gmail.com>
2022-11-26 00:58:23 +0300

    D'n'd v4
    Make a functionality to drag an object to another slot

--- Labyrinth and Minotaur/Assets/InventoryBtn.cs
+++ Labyrinth and Minotaur/Assets/InventoryBtn.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class InventoryBtn : MonoBehaviour, IDropHandler
 {
+    public Image img;
+
     public void OnDrop(PointerEventData eventData)
     {
-        print("Работаем в кнопке слота");
         GameObject dropped = eventData.pointerDrag;
         DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
         draggableItem.parentAfterDrag = transform;
--- Labyrinth and Minotaur/Assets/Scripts/Inventory/DraggableItem.cs
+++ Labyrinth and Minotaur/Assets/Scripts/Inventory/DraggableItem.cs
@@ -8,14 +8,23 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 {
     public Image image;
     [HideInInspector] public Transform parentAfterDrag;
-    
-        public void OnBeginDrag(PointerEventData eventData)
+    [HideInInspector] public Transform parentBeforeDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
         {
             //Debug.Log("BeginDrag");
+            parentBeforeDrag = transform.parent;
             parentAfterDrag = transform.parent;
+            //print("Родитель: " + parentAfterDrag);
+            //print("Старший родитель: " + parentAfterDrag.parent);
             transform.SetParent(transform.root);
             transform.SetAsLastSibling();
             image.raycastTarget = false;
+            
+            print(parentBeforeDrag.tag);
+            Transform newCurrentItemImg = Object.Instantiate(transform, transform, true);
+            newCurrentItemImg.GetComponent<Image>().sprite = null;
+            newCurrentItemImg.SetParent(parentBeforeDrag);
         }
     
         public void OnDrag(PointerEventData eventData)
@@ -26,8 +35,13 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     
         public void OnEndDrag(PointerEventData eventData)
         {
-            //Debug.Log("End drag");
+            
             transform.SetParent(parentAfterDrag);
+            
             image.raycastTarget = true;
+            if (parentAfterDrag.childCount > 1)
+            {
+                GameObject.Destroy(parentAfterDrag.GetChild(0).gameObject);
+            }
         }
 }
