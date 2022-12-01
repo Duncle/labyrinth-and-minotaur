2f57babad3fe4294f67cde53997aa2b6f75a6b5b
Duncle <solekawp1488@gmail.com>
2022-12-02 02:13:27 +0300

    Make equip slots work well

--- Labyrinth and Minotaur/Assets/Scripts/Inventory/DraggableItem.cs
+++ Labyrinth and Minotaur/Assets/Scripts/Inventory/DraggableItem.cs
@@ -7,35 +7,58 @@ using UnityEngine.UI;
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Image image;
+    public Sprite armorImg;
+    public Sprite weaponImg;
+    
     [HideInInspector] public Transform parentAfterDrag;
     [HideInInspector] public Transform parentBeforeDrag;
 
     public void OnBeginDrag(PointerEventData eventData)
         {
-            //Debug.Log("BeginDrag");
             parentBeforeDrag = transform.parent;
             parentAfterDrag = transform.parent;
-            //print("Родитель: " + parentAfterDrag);
-            //print("Старший родитель: " + parentAfterDrag.parent);
             transform.SetParent(transform.root);
             transform.SetAsLastSibling();
             image.raycastTarget = false;
             
-            //print(parentBeforeDrag.tag);
-            Transform newCurrentItemImg = Object.Instantiate(transform, transform, true);
-            newCurrentItemImg.GetComponent<Image>().sprite = null;
-            newCurrentItemImg.SetParent(parentBeforeDrag);
+            //Update equip slots sprites without an item in it
+            if (parentBeforeDrag.parent.tag == "ArmorSlot")
+            {
+                Transform newCurrentItemImg = Object.Instantiate(transform, transform, true);
+                newCurrentItemImg.GetComponent<Image>().sprite = armorImg;
+                newCurrentItemImg.SetParent(parentBeforeDrag);
+            }
+            else if (parentBeforeDrag.parent.tag == "WeaponSlot")
+            {
+                Transform newCurrentItemImg = Object.Instantiate(transform, transform, true);
+                newCurrentItemImg.GetComponent<Image>().sprite = weaponImg;
+                newCurrentItemImg.SetParent(parentBeforeDrag);
+            }
+            else
+            {
+                Transform newCurrentItemImg = Object.Instantiate(transform, transform, true);
+                newCurrentItemImg.GetComponent<Image>().sprite = null;
+                newCurrentItemImg.SetParent(parentBeforeDrag);
+            }
         }
     
         public void OnDrag(PointerEventData eventData)
         {
-            //Debug.Log("Dragging");
             transform.position = Input.mousePosition;
         }
     
         public void OnEndDrag(PointerEventData eventData)
         {
             if (parentAfterDrag.GetChild(0).GetComponent<Image>().sprite == null)
+            {
+                transform.SetParent(parentAfterDrag);
+                image.raycastTarget = true;
+                if (parentAfterDrag.childCount > 1)
+                {
+                    GameObject.Destroy(parentAfterDrag.GetChild(0).gameObject);
+                }
+            }
+            else if (parentAfterDrag.parent.CompareTag("ArmorSlot") || parentAfterDrag.parent.CompareTag("WeaponSlot"))
             {
                 transform.SetParent(parentAfterDrag);
                 image.raycastTarget = true;
